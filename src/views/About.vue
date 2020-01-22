29920145267e46fbfd30de0dbceb4a9ad1896526
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-22 20:14:27 +0200

    Navigation from JS code

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <button @click="goHome()">Home</button>
+    <router-link tag="button" to="/">Home</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      //console.log("Router", this.$router);
+      //console.log("Route", this.$route);
+      this.$router.push('/');
+    },
+  },
+}
+</script>

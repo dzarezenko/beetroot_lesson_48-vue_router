4be47134ff8e0920f985ed4b2ad190dbfe73582c
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-22 20:48:12 +0200

    Watch route

--- src/components/user/UserDetails.vue
+++ src/components/user/UserDetails.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>User Details</h1>
+  <div>
+    <h1>User Details</h1>
+    <p>User ID: {{ userId }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.id,
+    }
+  },
+  watch: {
+    $route(to, from) {
+      //console.log("TO", to);
+      //console.log("FROM", from);
+
+      this.userId = to.params.id;
+    }
+  }
+}
+</script>
